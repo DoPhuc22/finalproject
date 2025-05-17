9569b77e877c580e4cb0f47fdea1f0b75b4c274d
Do Hong Phuc <phucjzaw@gmail.com>
2025-05-17 17:10:53 +0700

    Fix bug: customer page content not displaying

--- src/layouts/CustomerLayout.jsx
+++ src/layouts/CustomerLayout.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from '../components/customer/Header/Header';
 import Footer from '../components/customer/Footer/Footer';
 
-const CustomerLayout = ({ children }) => {
+const CustomerLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow mb-12">{children}</main>
+      <main className="flex-grow mb-12">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
